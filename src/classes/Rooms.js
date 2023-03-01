439807a81f5805259e7293beb45d94eedc75f695
Travis Parks <travisparks0788@gmail.com>
2023-02-28 20:04:32 -0500

    Add starting layout and build out

---
+++ src/classes/Rooms.js
@@ -0,0 +1,13 @@
+class Rooms {
+  constructor(rooms) {
+    this.number = rooms.number;
+    this.roomType = rooms.roomType;
+    this.bidet = rooms.bidet;
+    this.bedSize = rooms.bedSize;
+    this.numBeds = rooms.numBeds;
+    this.costPerNight = rooms.costPerNight;
+  }
+}
+
+
+export default Rooms;
