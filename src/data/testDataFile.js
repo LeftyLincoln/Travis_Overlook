cc5c90ca5832e677bf1106a53f15510c914ff97b
Travis Parks <travisparks0788@gmail.com>
2023-03-03 13:42:19 -0500

    Add tests for CustomerClass

--- src/data/testDataFile.js
+++ src/data/testDataFile.js
@@ -13,53 +13,53 @@ const testCustomers = [
   }
 ]
 
-const bookings = [
+const testBookings = [
   {
-    "id": "5fwrgu4i7k55hl6sz",
-    "userID": 9,
-    "date": "2022/04/22",
-    "roomNumber": 15
+    "id": "5fwrgu4i7k55hl6uf",
+    "userID": 2,
+    "date": "2023/01/09",
+    "roomNumber": 18
   },
   {
-    "id": "5fwrgu4i7k55hl6t5",
-    "userID": 43,
-    "date": "2022/01/24",
-    "roomNumber": 24
+    "id": "5fwrgu4i7k55hl6vw",
+    "userID": 2,
+    "date": "2023/02/11",
+    "roomNumber": 9
   },
   {
-    "id": "5fwrgu4i7k55hl6t6",
-    "userID": 13,
-    "date": "2022/01/10",
-    "roomNumber": 12
-  }
+    "id": "5fwrgu4i7k55hl6x8",
+    "userID": 1,
+    "date": "2023/01/11",
+    "roomNumber": 20
+  },
 ]
 
-const rooms = [
+const testRooms = [
   {
-    "number": 1,
-    "roomType": "residential suite",
+    "number": 18,
+    "roomType": "junior suite",
+    "bidet": false,
+    "bedSize": "king",
+    "numBeds": 2,
+    "costPerNight": 496.41
+  },
+  {
+    "number": 9,
+    "roomType": "single room",
     "bidet": true,
     "bedSize": "queen",
     "numBeds": 1,
-    "costPerNight": 358.4
+    "costPerNight": 200.39
   },
   {
-    "number": 2,
-    "roomType": "suite",
+    "number": 20,
+    "roomType": "residential suite",
     "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 477.38
-  },
-  {
-    "number": 3,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "king",
+    "bedSize": "queen",
     "numBeds": 1,
-    "costPerNight": 491.14
+    "costPerNight": 343.95
   },
 ]
 
 
-export default {testCustomers, bookings, rooms};
+export default {testCustomers, testBookings, testRooms};
