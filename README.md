61eac1618046001aff99499abd272ba7d01b4072
Travis Parks <travisparks0788@gmail.com>
2023-03-06 13:21:37 -0500

    Add refactored syntax

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # Abstract
-- Overlook! This app was was built to let a customer sign in with a unique username, which brings them to the main page of a hotel booking site. Here you are abe to search for rooms by a date, as well as filter down the rooms available through the style of room. When you sign in, the customer is greeted with their name, and the amount they have spent at the hotel. The cut  
+
+- Overlook! This app was was built to let a customer sign in with a unique username, which brings them to the main page of a hotel booking site. Here you are abe to search for rooms by a date, as well as filter down the rooms available through the style of room. When you sign in, the customer is greeted with their name, and the amount they have spent at the hotel. The cut
 
 # Technologies
+
 - JavaScript
 - CSS
 - HTML
 - Webpack
-- Mocha / Chai 
+- Mocha / Chai
 
 # App Preview
+
 ![Overlook](https://user-images.githubusercontent.com/116752855/223187696-d34c1d6c-ff4e-459c-98e5-89ca86fb6c13.gif)
 
 # Challenges
+
 - Utilizing fetch API for GET and POST
 - Retrieving and filtering through all the data to display the desired information
 - Creating a log in page, that allows then a customer to see specific data related to them.
 
-# Contributors 
+# Contributors
+
 - [Travis Parks](https://github.com/LeftyLincoln)
 
 # Getting Started
+
 - [Clone down the API repository](https://github.com/turingschool-examples/overlook-api)
 - Clone down this repository to your local machine
 - Copy code
@@ -31,5 +37,5 @@
 - Enjoy!
 
 # Project Spec
-The details of this project are outlined [here.](https://frontend.turing.edu/projects/overlook.html) 
 
+The details of this project are outlined [here.](https://frontend.turing.edu/projects/overlook.html)
--- test/Rooms-test.js
+++ test/Rooms-test.js
@@ -1,51 +1,47 @@
-import chai from 'chai';
-import Rooms from '../src/classes/Rooms';
+import chai from "chai";
+import Rooms from "../src/classes/Rooms";
 const expect = chai.expect;
-import testData from '../src/data/testDataFile';
+import testData from "../src/data/testDataFile";
 
-
-describe('Rooms', () => {
-  let testRooms, room1, room2
+describe("Rooms", () => {
+  let testRooms, room1, room2;
   beforeEach(() => {
-    testRooms = testData.testRooms
-    room1 = new Rooms(testRooms[0])
-    room2 = new Rooms(testRooms[1])
+    testRooms = testData.testRooms;
+    room1 = new Rooms(testRooms[0]);
+    room2 = new Rooms(testRooms[1]);
   });
 
-  it('should be a function', () => {
+  it("should be a function", () => {
     expect(room1).to.be.instanceOf(Rooms);
   });
 
-  it('should have a unique number', () => {
+  it("should have a unique number", () => {
     expect(room1.number).to.equal(18);
     expect(room2.number).to.equal(9);
   });
 
-  
-  it('should have a specific roomType', () => {
-    expect(room1.roomType).to.equal( "junior suite");
-    expect(room2.roomType).to.equal( "single room");
+  it("should have a specific roomType", () => {
+    expect(room1.roomType).to.equal("junior suite");
+    expect(room2.roomType).to.equal("single room");
   });
 
-
-  it('should say if it has a bidet in the room', () => {
+  it("should say if it has a bidet in the room", () => {
     expect(room1.bidet).to.equal(false);
     expect(room2.bidet).to.equal(true);
   });
 
-  it('should specify the bedsize', () => {
+  it("should specify the bedsize", () => {
     expect(room1.bedSize).to.equal("king");
     expect(room2.bedSize).to.equal("queen");
   });
 
-  it('should specify the number of beds', () => {
+  it("should specify the number of beds", () => {
     expect(room1.numBeds).to.equal(2);
     expect(room2.numBeds).to.equal(1);
   });
 
-  it('should specify the cost of the room for a night', () => {
+  it("should specify the cost of the room for a night", () => {
     expect(room1.costPerNight).to.equal(496.41);
     expect(room2.costPerNight).to.equal(200.39);
   });
-
 });
